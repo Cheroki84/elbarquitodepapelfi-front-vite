2a533b14fc02fd6c52a1c1e29c7f5b73d9b68242
Miguel Ángel Morato Cadenas <miguelangelmorato84@gmail.com>
2022-11-01 23:52:07 +0100

    completed Vite configuration (dependencies and test files included)

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,28 +1,22 @@
-import PropTypes from 'prop-types';
-import moment from 'moment';
+import PropTypes from 'prop-types'
+import moment from 'moment'
 
 export const Welcome = ({ userName }) => {
-    const hour = Number(moment().format('H'));
+	const hour = Number(moment().format('H'))
 
-    if (hour >= 8 && hour < 12) {
-        return (
-            <div>{`Buenos días ${userName} `}</div>
-        )
-    } else if (hour >= 12 && hour < 20) {
-        return (
-            <div>{`Buenas tardes ${userName} `}</div>
-        )
-    } else {
-        return (
-            <div>{`Buenas noches ${userName} `}</div>
-        )
-    }
+	if (hour >= 8 && hour < 12) {
+		return <div>{`Buenos días ${userName} `}</div>
+	} else if (hour >= 12 && hour < 20) {
+		return <div>{`Buenas tardes ${userName} `}</div>
+	} else {
+		return <div>{`Buenas noches ${userName} `}</div>
+	}
 }
 
 Welcome.propTypes = {
-    userName: PropTypes.string.isRequired
+	userName: PropTypes.string.isRequired,
 }
 
 Welcome.defaultProps = {
-    userName: 'anónimo'
+	userName: 'anónimo',
 }
---
+++ tests/components/Welcome.test.jsx
@@ -0,0 +1,5 @@
+describe('Pruebas sobe el componente Welcome', () => {
+	test('Debe devolver un saludo', () => {
+		expect(1).toBe(1)
+	})
+})
