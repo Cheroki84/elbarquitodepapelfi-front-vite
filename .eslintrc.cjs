f6bcd7ae1d0703a2be6ef03825ae5aa0255fe312
Miguel Ángel Morato Cadenas <miguelangelmorato84@gmail.com>
2022-11-18 20:25:29 +0100

    format eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,19 @@ module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
-		jest: true
+		jest: true,
 	},
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-	extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'standard', 'eslint-config-prettier'],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier',
+	],
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
